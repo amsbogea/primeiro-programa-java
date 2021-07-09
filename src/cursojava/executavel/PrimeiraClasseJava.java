c0f6437579f04d0e1a17030bf4f19cb684f55068
Alex Miranda <alex_miranda@outlook.com.br>
2021-07-09 15:37:15 -0300

    if else com char

--- src/cursojava/executavel/PrimeiraClasseJava.java
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -5,14 +5,31 @@ public class PrimeiraClasseJava {
 	//main é um método auto executável em Java
 	public static void main(String[] args) {
 		
-		double nota1 = 90;   
-		double nota2 = 70;
-		double nota3 = 50;
-		double nota4 = 88;
+		/*Código comentado para fins de estudos
+		 * 
+			double nota1 = 90;   
+			double nota2 = 70;
+			double nota3 = 50;
+			double nota4 = 88;
 		
-		double mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
+			double mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
 		
-		System.out.println("Média das notas " + mediaFinal);
+			System.out.println("Média das notas " + mediaFinal);
+		*
+		*/
+		
+		//Char representa uma letra
+			char pessoaFeminino = 'F';
+			
+			if (pessoaFeminino == 'F') {
+				System.out.println("Feminino");
+			}else {
+				System.out.println("Masculino");
+			}
+	
 	}
+	
+	
+	
 
 }
