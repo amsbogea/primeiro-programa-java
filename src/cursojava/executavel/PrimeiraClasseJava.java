7bf1b00c284d321ebf485b9f93def6ab8f37cfb1
Alex Miranda <alex_miranda@outlook.com.br>
2021-07-09 15:40:13 -0300

    PrimeiraClasseJava2 criada afim de separar os códigos

--- src/cursojava/executavel/PrimeiraClasseJava.java
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -5,8 +5,7 @@ public class PrimeiraClasseJava {
 	//main é um método auto executável em Java
 	public static void main(String[] args) {
 		
-		/*Código comentado para fins de estudos
-		 * 
+		
 			double nota1 = 90;   
 			double nota2 = 70;
 			double nota3 = 50;
@@ -15,17 +14,7 @@ public class PrimeiraClasseJava {
 			double mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
 		
 			System.out.println("Média das notas " + mediaFinal);
-		*
-		*/
 		
-		//Char representa uma letra
-			char pessoaFeminino = 'F';
-			
-			if (pessoaFeminino == 'F') {
-				System.out.println("Feminino");
-			}else {
-				System.out.println("Masculino");
-			}
 	
 	}
 	
---
+++ src/cursojava/executavel/PrimeiraClasseJava2.java
@@ -0,0 +1,22 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava2 {
+
+	//main é um método auto executável em Java
+	public static void main(String[] args) {
+		
+			//Char representa uma letra
+			char pessoaFeminino = 'F';
+			
+			if (pessoaFeminino == 'F') {
+				System.out.println("Feminino");
+			}else {
+				System.out.println("Masculino");
+			}
+	
+	}
+	
+	
+	
+
+}
