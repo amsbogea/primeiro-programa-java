44e4276cc21a6575d7c2a47cc410c1900c4a55a9
Alex Miranda <alex_miranda@outlook.com.br>
2021-07-12 12:02:55 -0300

    Nova classe java

---
+++ src/cursojava/executavel/PrimeiraClasseJava3.java
@@ -0,0 +1,17 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava3 {
+
+	//main é um método auto executável em Java
+	public static void main(String[] args) {
+		
+			//Char representa uma letra
+			char pessoaFeminino = 'F';
+			
+			if (pessoaFeminino == 'F') {
+				System.out.println("Feminino");
+			}else {
+				System.out.println("Masculino");
+			}	
+	}
+}
