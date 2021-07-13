1a427e9de82068c09232482a9ebb9f356ce06e8c
Alex Miranda <alex_miranda@outlook.com.br>
2021-07-12 21:25:17 -0300

    AULA - Operações lógicas com IF e ELSE no linux em casa

--- src/cursojava/executavel/PrimeiraClasseJava2.java
+++ src/cursojava/executavel/PrimeiraClasseJava2.java
@@ -2,7 +2,11 @@ package cursojava.executavel;
 
 public class PrimeiraClasseJava2 {
 
+<<<<<<< Updated upstream
 	//main  um  método auto executável em Java
+=======
+	//main � um m�todo auto execut�vel em Java
+>>>>>>> Stashed changes
 	public static void main(String[] args) {
 		
 			//Char representa uma letra
